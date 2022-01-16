4665556f42aece1f3ffb7190a14d6911964f47d0
Matt Burgess <matt@myclient.info>
2022-01-16 23:23:23 +0700

    Finish second day requirement

--- rust02/src/main.rs
+++ rust02/src/main.rs
@@ -1,3 +1,32 @@
+enum Directions {
+    Forward,
+    Up,
+    Down,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut depth = 0;
+    let mut position = 0;
+
+    let inputs = [
+        (Directions::Forward, 5),
+        (Directions::Down, 5),
+        (Directions::Forward, 8),
+        (Directions::Up, 3),
+        (Directions::Down, 8),
+        (Directions::Forward, 2),
+    ];
+
+    for input in inputs.iter() {
+        let (dir, num) = input;
+        match dir {
+            Directions::Forward => position += num,
+            Directions::Up => depth -= num,
+            Directions::Down => depth += num,
+        }
+    }
+
+    println!("Current Position: {} down, {} across", depth, position);
+
+    println!("Position Multiplier: {}", depth * position);
 }
